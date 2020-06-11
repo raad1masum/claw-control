78bf4999d251e073f1e8f7d09260f05e97aef662
raad1masum <piraadwan@gmail.com>
2020-06-11 10:03:20 -0700

    remove debugging print statements

--- src/dead_reckoning/dead_reckoning.go
+++ src/dead_reckoning/dead_reckoning.go
@@ -11,16 +11,12 @@ func main() {
 	reader1 := bufio.NewReader(os.Stdin)
     fmt.Print("Enter Distance: ")
 	s1, _ := reader1.ReadString('\n')
-	fmt.Println(s1)
 	distance, _ := strconv.ParseFloat(s1, 64)
-	fmt.Println(distance)
 
 	reader2 := bufio.NewReader(os.Stdin)
     fmt.Print("Enter Velocity: ")
 	s2, _ := reader2.ReadString('\n')
-	fmt.Println(s2)
 	velocity, _ := strconv.ParseFloat(s2, 64)
-	fmt.Println(velocity)
 
 	fmt.Println("Output:", deadReckoning(distance, velocity))
 
