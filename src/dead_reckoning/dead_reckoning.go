159fd8cb2b1971ad7c952940e077b80361ca3071
raad1masum <piraadwan@gmail.com>
2020-06-11 10:03:04 -0700

    get input from user

--- src/dead_reckoning/dead_reckoning.go
+++ src/dead_reckoning/dead_reckoning.go
@@ -2,15 +2,28 @@ package main
 
 import (
 	"bufio"
-	"fmt"
+    "fmt"
 	"os"
+	"strconv"
 )
 
 func main() {
-	distance := 10.0
-	velocity := 5.0
-	
+	reader1 := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter Distance: ")
+	s1, _ := reader1.ReadString('\n')
+	fmt.Println(s1)
+	distance, _ := strconv.ParseFloat(s1, 64)
+	fmt.Println(distance)
+
+	reader2 := bufio.NewReader(os.Stdin)
+    fmt.Print("Enter Velocity: ")
+	s2, _ := reader2.ReadString('\n')
+	fmt.Println(s2)
+	velocity, _ := strconv.ParseFloat(s2, 64)
+	fmt.Println(velocity)
+
 	fmt.Println("Output:", deadReckoning(distance, velocity))
+
 }
 
 func deadReckoning(distance float64, velocity float64) float64 {
