b935061a929db5c07e4bf48b4fb5443a659171ad
raad1masum <piraadwan@gmail.com>
2020-06-11 09:38:13 -0700

    add output string to output

--- src/dead_reckoning/dead_reckoning.go
+++ src/dead_reckoning/dead_reckoning.go
@@ -8,7 +8,7 @@ func main() {
 	distance := 10.0
 	velocity := 5.0
 	
-	fmt.Println(deadReckoning(distance, velocity))
+	fmt.Println("Output:", deadReckoning(distance, velocity))
 }
 
 func deadReckoning(distance float64, velocity float64) float64 {
