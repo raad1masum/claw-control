fa8c32393cafb7838afc1d0faf10bb8c2daae12b
raad1masum <piraadwan@gmail.com>
2020-06-13 11:54:13 -0700

    add exit messgae

--- src/bang_bang/bang_bang.go
+++ src/bang_bang/bang_bang.go
@@ -20,6 +20,7 @@ func bangBang(targetDistance float64, currentDistance float64) {
 			varError++
 			fmt.Println(varError)
 		} else if varError == 0 {
+			fmt.Print("Setpoint achieved, exiting controller: ")
 			os.Exit(1)
 		}
 	}
