358fbcb07c54df5e417d7e00c1f306ade58447c0
raad1masum <piraadwan@gmail.com>
2020-06-15 12:01:17 -0700

    add user input to proportional

--- src/PID/proportional.go
+++ src/PID/proportional.go
@@ -5,7 +5,32 @@ import (
 )
 
 func proportionalController() {
-	proportional(10.0, 5.0, 1.0)
+	var f1 float64
+	var f2 float64
+	var f3 float64
+
+	fmt.Println("Enter Setpoint: ")
+	_, err1 := fmt.Scanf("%f1", &f1)
+
+	if err1 != nil {
+		fmt.Println(" ") 
+	}
+
+	fmt.Println("Enter Target Position: ")
+	_, err2 := fmt.Scanf("%f2", &f2)
+
+	if err2 != nil {
+		fmt.Println(" ") 
+	}
+
+	fmt.Println("Enter kP gain: ")
+	_, err3 := fmt.Scanf("%f3", &f3)
+
+	if err3 != nil {
+		fmt.Println(" ") 
+	}
+
+	proportional(f1, f2, f3)
 }
 
 func proportional(setpoint float64, currentPosition float64, kP float64) {
