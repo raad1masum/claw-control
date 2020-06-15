00b2153ab63f580996fb6e6cf9433cd6bf84377c
raad1masum <piraadwan@gmail.com>
2020-06-15 12:18:52 -0700

    add user input for derivative

--- src/PID/derivative.go
+++ src/PID/derivative.go
@@ -5,7 +5,32 @@ import (
 )
 
 func main() {
-	derivative(10.0, 5.0, 2.0)
+	var f1 float64
+	var f2 float64
+	var f3 float64
+
+	fmt.Println("Enter Setpoint: ")
+	_, err1 := fmt.Scanf("%f1", &f1)
+
+	if err1 != nil {
+		fmt.Println(" ") 
+	}
+
+	fmt.Println("Enter Current Position: ")
+	_, err2 := fmt.Scanf("%f2", &f2)
+
+	if err2 != nil {
+		fmt.Println(" ") 
+	}
+
+	fmt.Println("Enter kD gain: ")
+	_, err3 := fmt.Scanf("%f3", &f3)
+
+	if err3 != nil {
+		fmt.Println(" ") 
+	}
+
+	derivative(f1, f2, f3)
 }
 
 func derivative(setpoint float64, currentPosition float64, kD  float64) {
