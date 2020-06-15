6917ff38abc746d58d9730ef29fc80c8fb5d31b1
raad1masum <piraadwan@gmail.com>
2020-06-14 19:23:08 -0700

    add proportional controller

--- src/PID/proportional.go
+++ src/PID/proportional.go
@@ -6,4 +6,16 @@ import (
 
 func main() {
 	fmt.Println("PID FTW")
+	proportional(10.0, 5.0, 1.0)
+}
+
+func proportional(setpoint float64, currentPosition float64, kP float64) {
+	varError := setpoint - currentPosition
+	output := 0.0
+
+	if varError != 0 {
+		output = currentPosition * kP
+	}
+
+	fmt.Println("Output:", output)
 }
