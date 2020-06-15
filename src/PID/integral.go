7582aab8980c618987f3fc06a10ca1c574c60bf3
raad1masum <piraadwan@gmail.com>
2020-06-15 12:07:50 -0700

    add integral function

--- src/PID/integral.go
+++ src/PID/integral.go
@@ -5,9 +5,14 @@ import (
 )
 
 func main() {
-	fmt.Println("PID FTW")
+	integral(10.0, 5.0, 2.0)
 }
 
-func integral() {
-	fmt.Println("integral")
+func integral(setpoint float64, currentPosition float64, kI float64) {
+	varError := setpoint - currentPosition
+	integral := varError * 0.00001
+
+	output := integral * kI
+
+	fmt.Println("Output:", output)
 }
