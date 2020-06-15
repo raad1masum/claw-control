21bd541e45b469ad0538361459f12d4eaa2abd79
raad1masum <piraadwan@gmail.com>
2020-06-15 12:41:45 -0700

    create proportional adjust

--- src/PID/PID.go
+++ src/PID/PID.go
@@ -7,3 +7,14 @@ import (
 func main() {
 	fmt.Println("PID FTW")
 }
+
+func proportionalAdjust(setpoint float64, currentPosition float64, kP float64) float64 {
+	varError := setpoint - currentPosition
+	output := 0.0
+
+	if varError != 0 {
+		output = currentPosition * kP
+	}
+
+	return output
+}
